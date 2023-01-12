ba2ea10057803f22ddbe0b02c9a5a84f8a8d1a17
sardarz <muhkween@gmail.com>
2023-01-12 13:27:32 +0300

    add asset files and color styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
